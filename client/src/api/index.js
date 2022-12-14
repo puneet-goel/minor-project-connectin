2bcf0a7d590fa5d9ae415f94da067d6d784dc2f4
Puneet Goel <puneetgoel016@gmail.com>
2022-12-14 19:34:13 +0530

    api link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const URL = 'https://social-media-ctxa.onrender.com';
-const URL = 'http://localhost:5000';
+const URL = 'https://social-media-ctxa.onrender.com';
+// const URL = 'http://localhost:5000';
 
 const postUrl = URL + '/posts';
 const authUrl = URL + '/auth';
